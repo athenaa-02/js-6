a6aafb688e7718f4d90ba5e167543395fec319ad
natia <nkaramnishvili@itvet.ge>
2026-03-24 16:48:10 +0400

    comparison operators

---
+++ main.js
@@ -0,0 +1,43 @@
+//    =  - მინიჭების ოპერატორი
+let number = 20;
+
+console.log(number)
+
+//     ==   -  მნიშვნელობების შედარება
+let age = 30;
+let chosenAge = "30";
+
+let text1 = 'this is some text' 
+let text2 = 'this is some text'
+
+
+
+console.log(text1 == text2)
+// console.log(age == chosenAge);
+
+
+//     ===  - მნიშვნელობებს და ტიპებს ადარებს
+console.log(text1 === text2)
+
+
+console.log(age === chosenAge);
+
+
+console.log(20 >= 10)
+console.log(30 % 11)
+console.log(20 % 8 === 0 )
+
+
+
+
+console.log('natia ' + 'itvet')
+
+console.log(20 + '1sgdjka') 
+
+console.log('200' - 150)
+
+// + --- string
+// - / *   ---  number
+
+
+
